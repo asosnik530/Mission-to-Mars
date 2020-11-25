e843156fa256124bbfadbe7e2e2d2f6975330bd4
Your Name <asosnik530@yahoo.com>
2020-11-25 16:57:19 -0500

    add files

--- app.py
+++ app.py
@@ -15,6 +15,6 @@ def scrape():
    mars_data = scraping.scrape_all()
    mars.update({}, mars_data, upsert=True)
    return "Scraping Successful!"
-.update(query_parameter, data, options)
+update(query_parameter, data, options)
 if __name__ == "__main__":
    app.run()
